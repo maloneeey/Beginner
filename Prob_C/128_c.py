d31b3e8dbceb32f03711518d92225acf12500318
maloneeey <koutarou.brocade@gmail.com>
2019-10-07 00:42:11 +0900

    surf

--- Prob_C/128_c.py
+++ Prob_C/128_c.py
@@ -1,3 +1,4 @@
+from itertools import combinations_with_replacement
 n, m = map(int, input().split())
 s = []
 for _ in range(m):
@@ -5,3 +6,14 @@ for _ in range(m):
   s.append(s_)
 p = list(map(int, input().split()))
 
+onoff = [0, 1]
+cnt = 0
+for switch in combinations_with_replacement(onoff, n):
+  for i in range(m):
+    on = 0
+    for j in range(1, s[i][0]+1):
+      if switch[ s[i][j]-1 ] == 1:
+        on += 1
+    if on%2 == p[i]:
+      cnt += 1
+print(cnt)
