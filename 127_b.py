5ae71e31af27fce9ae67465351d448572b60c094
maloneeey <koutarou.brocade@gmail.com>
2019-10-08 01:14:45 +0900

    sur

---
+++ 127_b.py
@@ -0,0 +1,5 @@
+s = list(input())
+left = int( s[0]+s[1] )
+right = int( s[2]+s[3] )
+
+if left < 
--- Prob_C/128_c.py
+++ Prob_C/128_c.py
@@ -1,19 +1,22 @@
-from itertools import combinations_with_replacement
+from itertools import permutations
 n, m = map(int, input().split())
 s = []
 for _ in range(m):
-  s_ = list(map(int, input().split()))
-  s.append(s_)
+    s_ = list(map(int, input().split()))
+    s.append(s_)
 p = list(map(int, input().split()))
 
-onoff = [0, 1]
 cnt = 0
-for switch in combinations_with_replacement(onoff, n):
-  for i in range(m):
-    on = 0
-    for j in range(1, s[i][0]+1):
-      if switch[ s[i][j]-1 ] == 1:
-        on += 1
-    if on%2 == p[i]:
-      cnt += 1
+for i in range(pow(2, n)):
+    switch = [i>>j&1 for j in range(n)]
+    light = 0
+    for j in range(m):
+        on = 0
+        for k in range(1, s[j][0]+1):
+            if switch[k-1]==1:
+                on += 1
+        if on%2 == p[j]:
+            light += 1
+    if light == m:
+        cnt += 1
 print(cnt)
